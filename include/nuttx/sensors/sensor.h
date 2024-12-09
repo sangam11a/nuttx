c3bb212d807fb8b9abe9127ee362d140af1044e6
sangam218 <sangam.thapa218@gmail.com>
2024-12-09 18:46:08 +0545

    had some improvement in reservation command with implementation of sort and deletion in same code rather than from storage manager

--- include/nuttx/sensors/sensor.h
+++ include/nuttx/sensors/sensor.h
@@ -538,9 +538,70 @@ struct sensor_temp          /* Type: Ambient Temperature */
 struct sensor_rgb           /* Type: RGB */
 {
   uint64_t timestamp;       /* Units is microseconds */
-  float r;                  /* Units is percent */
-  float g;                  /* Units is percent */
-  float b;                  /* Units is percent */
+  // float r;                  /* Units is percent */
+  // float g;                  /* Units is percent */
+  // float b;                  /* Units is percent */
+   int16_t accl_x;
+  int16_t accl_y;
+  int16_t accl_z;
+  int16_t gyro_x;
+  int16_t gyro_y;
+  int16_t gyro_z;
+  int16_t mag_x;
+  int16_t mag_y;
+  int16_t mag_z;
+  int16_t temp_x;
+  int16_t temp_x1;
+  int16_t temp_y;
+  int16_t temp_y1;
+  int16_t temp_z;
+  int16_t temp_z1;
+  int16_t temp_bpb;
+  int16_t temp_obc;
+  int16_t temp_com;
+  int16_t temp_batt;
+  int16_t batt_volt;
+  int16_t sol_p1_v;
+  int16_t sol_p2_v;
+  int16_t sol_p3_v;
+  int16_t sol_p4_v;
+  int16_t sol_p5_v;
+  int16_t sol_t_v;
+  int16_t raw_v;
+  int16_t sol_p1_c;
+  int16_t sol_p2_c;
+  int16_t sol_p3_c;
+  int16_t sol_p4_c;
+  int16_t sol_p5_c;
+  int16_t sol_t_c;
+  int16_t rst_3v3_c;
+  int16_t raw_c;
+  int16_t v3_main_c;
+  int16_t v3_com_c;
+  int16_t v3_2_c;
+  int16_t v5_c;
+  int16_t unreg_c;
+  int16_t v4_c;
+  int16_t batt_c;
+  int8_t rsv_cmd;
+
+  int8_t ant_dep_stat;
+  int8_t ul_state;
+  int8_t oper_mode;
+  int8_t msn_flag;
+  int8_t rsv_flag;
+  int8_t kill_switch;
+
+  int16_t ant_temp_out;
+};
+
+struct reservation_command{
+  uint8_t mcu_id;
+	uint8_t cmd[3];
+	uint8_t time[2];
+	uint16_t latest_time;
+  bool executed;
+
 };
 
 struct sensor_hall          /* Type: HALL */
